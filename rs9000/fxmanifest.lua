1493175a5927ff86edd78bf13cd01c131fd5a3fc
Greve <frankgrevekrogh@gmail.com>
2025-01-29 11:33:37 +0100

    feat:(refactor): Updated Manifest & Refactor Folder
    Updated and refactored folders and manifest

---
+++ rs9000/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+ui_page 'nui/index.html'
+
+files {
+	'config.json',
+	'nui/index.html',
+	'nui/js/index.js',
+	'nui/css/style.css',
+	'nui/fonts/d-din.otf',
+	'nui/fonts/dseg7.ttf',
+	'modules/Newtonsoft.Json.dll',
+}
+
+client_script 'modules/RS9000.net.dll'
